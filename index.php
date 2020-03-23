be11dbd9e9e9f48cf86e29f2225c5588924ce52c
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2020-03-22 21:58:19 -0300

    Refactor index.php, creating a better stack trace
    Was created a better stack trace, with objective
    to improve the reading gave by the exceptions

--- index.php
+++ index.php
@@ -3,31 +3,32 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require 'Validation.php';
-require 'CheckingAccount.php';
+require_once 'autoload.php';
+
+use Validation;
+use CheckingAccount;
+
 $johnAccount = new CheckingAccount('John', 1010, '1010-10', 2000.00);
-
-$mariaAccount = new CheckingAccount('Maria', 2010, '4410-10', 20000.00);
-
-
-$josephAccount = new CheckingAccount('Joseph', 2810, '5510-10', 45000.00);
+$mariaAccount = new CheckingAccount('Maria', 5050, '5050-10', 3000.00);
+$johnAccount->deposit(100);
 
 
-
-
-
-
-
+echo PHP_EOL;
 try { 
 	$johnAccount->transfer(true, $mariaAccount);
-} catch(Exception $e) {
-	echo $e->getMessage();
+} catch(\exception\InsufficientBalanceException $e) {
+	$johnAccount->withdrawalsNotAlloweds++;
+	echo $e->getMessage() . " | Balance into account: " . $e->balance . " | Value of withdraw: " . $e->value ;
+} catch(\exception\OperationNotRealizedException $e) {
+	echo $e->getMessage();	
+} catch(\Exception $e) {
+		//echo $e->getPrevious()->getMessage();
+		echo $e->getPrevious()->getTraceAsString();
+
 }
 
 echo PHP_EOL;
 
-try { 
-	$johnAccount->transfer(0, $mariaAccount);
-} catch(Exception $e) {
-	echo $e->getMessage();
-}
+echo "Operations not realizeds: " . CheckingAccount::$operationNotRealized;
+
+var_dump($johnAccount);
